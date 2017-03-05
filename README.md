316f5886fe20dd0b1b6112f70542b1848bb15531
Rob Calcroft <rob@calcroft.com>
2017-03-05 22:11:21 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+#plex-webhook-recently-added
+> Runs a webhook with recently added items from your Plex server
+
+##Install
+1. Clone the repo
+2. `yarn`
+3. Use `.env-sample` to create your own `.env`
+4. Use `cron` to run the task every `X (days|minutes|hours etc)` along with configuring the `UPDATED_AT_FILTER_VALUE` and `UPDATED_AT_FILTER_UNIT` to match
+5. Configure your webhook(s) to receive the JSON payload and do with it what you will
---
+++ index.js
@@ -0,0 +1,88 @@
+require('dotenv').load();
+const info = require('./package.json');
+const request = require('request');
+const moment = require('moment');
+
+const debug = process.env.NODE_ENV === 'production' ? () => {} : console.log; // eslint-disable-line no-console
+const signInUrl = process.env.SIGN_IN_URL || 'https://plex.tv/users/sign_in.json';
+
+if (!process.env.WEBHOOKS || process.env.WEBHOOKS.length < 1) {
+  throw new Error('No webhook(s) provided! Add them in .env');
+}
+
+if (!process.env.PLEX_URL) {
+  throw new Error('No Plex URL provided! Add it in .env');
+}
+
+if (!process.env.USERNAME) {
+  throw new Error('No Plex username provided! Add it in .env');
+}
+
+if (!process.env.PASSWORD) {
+  throw new Error('No Plex password provided! Add it in .env');
+}
+
+debug(`${new Date()}`, `Requesting ${signInUrl}`);
+
+request.post({
+  url: signInUrl,
+  form: {
+    'user[login]': process.env.USERNAME,
+    'user[password]': process.env.PASSWORD,
+  },
+  headers: {
+    'X-Plex-Client-Identifier': process.env.UUID || '48a766bc-45ac-40e0-a0f6-c62a6bce42d4',
+    'X-Plex-Product': info.name,
+    'X-Plex-Version': info.version,
+    'X-Plex-Device-Name': process.env.NAME || info.name,
+    Accept: 'application/json',
+  },
+}, (loginError, loginResult, login) => {
+  if (loginError) {
+    return debug(`${new Date()}`, `Error retrieving token, status ${loginResult.statusCode} - `, loginError);
+  }
+
+  const user = JSON.parse(login).user;
+  const token = user.authentication_token;
+  const port = process.env.PLEX_PORT || 32400;
+  const url = `${process.env.PLEX_URL}:${port}/library/recentlyAdded`;
+
+  debug(`${new Date()}`, `Requesting ${url}`);
+
+  return request({
+    url,
+    headers: {
+      'X-Plex-Token': token,
+      Accept: 'application/json',
+    },
+  }, (recentlyAddedError, recentlyAddedResult, recentlyAdded) => {
+    if (recentlyAddedError) {
+      return debug(`${new Date()}`, `Error getting recently added, status ${recentlyAddedResult.statusCode} - `, recentlyAddedError);
+    }
+
+    const items = JSON.parse(recentlyAdded).MediaContainer.Metadata;
+    const oneDayAgo = moment().subtract(process.env.UPDATED_AT_FILTER_VALUE || 1, process.env.UPDATED_AT_FILTER_UNIT || 'days');
+    const addedWithinLastDay = items.filter(item => moment.unix(item.addedAt).isAfter(oneDayAgo));
+    const formattedItems = addedWithinLastDay.map((item) => {
+      let name;
+      if (item.type === 'movie') {
+        name = item.title;
+      } else if (item.type === 'season') {
+        name = `${item.parentTitle} ${item.title}`;
+      } else {
+        name = `${item.title} (${item.type})`;
+      }
+      return name;
+    });
+
+    return JSON.parse(process.env.WEBHOOKS).forEach((webhook) => {
+      debug(`${new Date()}`, `Running webhook ${webhook}`);
+      request.post({
+        url: webhook,
+        timeout: 10000,
+        body: formattedItems,
+        json: true,
+      });
+    });
+  });
+});
